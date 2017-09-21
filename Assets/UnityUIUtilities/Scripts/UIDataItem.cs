c97bc936b6b7d23f9a08b23628d4868ddfb9fd76
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-09-22 04:14:03 +0700

    Add generic dialogs

---
+++ Assets/UnityUIUtilities/Scripts/UIDataItem.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum UIDataItemClickMode
+{
+    Default,
+    Selection,
+    Disable,
+}
+
+public abstract class UIDataItem<T> : UIBase where T : class
+{
+    // Decoration
+    public bool selected;
+    private bool dirtySelected;
+    public GameObject selectedObject;
+    // Events
+    public UIDataItemClickMode clickMode = UIDataItemClickMode.Default;
+    public System.Action<UIDataItem<T>> eventClick;
+    public System.Action<UIDataItem<T>> eventSelect;
+    public System.Action<UIDataItem<T>> eventDeselect;
+    // Data
+    public T data;
+    private T dirtyData;
+
+    public virtual bool Selected
+    {
+        get { return selected; }
+        set { selected = value; }
+    }
+
+    public bool IsDirty()
+    {
+        return data != dirtyData;
+    }
+
+    private void Update()
+    {
+        if (IsDirty())
+            UpdateLogic();
+
+        if (Selected != dirtySelected)
+        {
+            if (selectedObject != null)
+                selectedObject.SetActive(Selected);
+            dirtySelected = Selected;
+        }
+    }
+
+    private void UpdateLogic()
+    {
+        Clear();
+        UpdateData();
+        dirtyData = data;
+    }
+
+    public void ForceUpdate()
+    {
+        UpdateLogic();
+    }
+
+    public virtual void OnClick()
+    {
+        switch (clickMode)
+        {
+            case UIDataItemClickMode.Selection:
+                Selected = !Selected;
+                if (Selected)
+                    Select();
+                else
+                    Deselect();
+                break;
+            case UIDataItemClickMode.Default:
+                Click();
+                break;
+        }
+    }
+
+    public virtual void Select(bool invokeEvent = true)
+    {
+        Selected = true;
+        if (invokeEvent)
+            eventSelect(this);
+    }
+
+    public virtual void Deselect(bool invokeEvent = true)
+    {
+        Selected = false;
+        if (invokeEvent)
+            eventDeselect(this);
+    }
+
+    public virtual void Click(bool invokeEvent = true)
+    {
+        Selected = false;
+        if (invokeEvent)
+            eventClick(this);
+    }
+
+    public abstract void UpdateData();
+    public abstract void Clear();
+}
---
+++ Assets/UnityUIUtilities/Scripts/UIList.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIList<T> : UIBase where T : MonoBehaviour
+{
+    public readonly Dictionary<string, T> Items = new Dictionary<string, T>();
+    public Transform container;
+    public T itemPrefab;
+
+    public virtual T SetItem(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+            return null;
+        if (Items.ContainsKey(id))
+            return Items[id];
+        var newItemObject = Instantiate(itemPrefab.gameObject);
+        var newItem = newItemObject.GetComponent<T>();
+        newItemObject.transform.SetParent(container);
+        newItemObject.transform.localScale = Vector3.one;
+        newItemObject.SetActive(true);
+        Items.Add(id, newItem);
+        return newItem;
+    }
+
+    public virtual bool RemoveItem(string id)
+    {
+        if (Items.ContainsKey(id))
+        {
+            var item = Items[id];
+            if (Items.Remove(id))
+            {
+                Destroy(item.gameObject);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public virtual void ClearItems()
+    {
+        foreach (var item in Items.Values)
+        {
+            Destroy(item.gameObject);
+        }
+        Items.Clear();
+    }
+}
