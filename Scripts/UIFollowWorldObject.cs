cbb4d18668296fd2e7ecbb29bd1c3dedde894c4c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-11-30 04:05:07 +0700

    Add World position follower for UIs

---
+++ Scripts/UIFollowWorldObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UIFollowWorldPosition))]
+public class UIFollowWorldObject : MonoBehaviour
+{
+    public GameObject targetObject;
+    private UIFollowWorldPosition tempPositionFollower;
+    public UIFollowWorldPosition TempPositionFollower
+    {
+        get
+        {
+            if (tempPositionFollower == null)
+                tempPositionFollower = new UIFollowWorldPosition();
+            return tempPositionFollower;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        TempPositionFollower.targetPosition = targetObject.transform.position;
+    }
+}
---
+++ Scripts/UIFollowWorldPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIFollowWorldPosition : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    private RectTransform tempTransform;
+    public RectTransform TempTransform
+    {
+        get
+        {
+            if (tempTransform == null)
+                tempTransform = GetComponent<RectTransform>();
+            return tempTransform;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = RectTransformUtility.WorldToScreenPoint(Camera.main, targetPosition);
+        TempTransform.position = pos;
+    }
+}
