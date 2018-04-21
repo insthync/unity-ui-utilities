7a464de05549c2837c33bb811811d2c65c854083
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-04-21 22:09:41 +0700

    WIP (Untested) animator ui extension

---
+++ Scripts/UIAnimatorExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class UIAnimatorExtension : BaseUIExtension
+{
+    public string animShowParam = "Show";
+    private Animator cacheAnimator;
+    public Animator CacheAnimator
+    {
+        get
+        {
+            if (cacheAnimator == null)
+                cacheAnimator = GetComponent<Animator>();
+            return cacheAnimator;
+        }
+    }
+
+    public override void Show()
+    {
+        CacheAnimator.SetBool(animShowParam, true);
+    }
+
+    public override void Hide()
+    {
+        CacheAnimator.SetBool(animShowParam, false);
+    }
+
+    public override bool IsVisible()
+    {
+        return CacheAnimator.GetBool(animShowParam);
+    }
+}
