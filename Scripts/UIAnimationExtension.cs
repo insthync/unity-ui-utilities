8905d3f0b6d05eb5290984da0ee27bb891114d7e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-16 16:39:12 +0700

    Add animation extension

---
+++ Scripts/UIAnimationExtension.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class UIAnimationExtension : BaseUIExtension
+{
+    public string animShowParam = "Show";
+    private bool isShowing;
+    private Animation cacheAnimation;
+    public Animation CacheAnimation
+    {
+        get
+        {
+            if (cacheAnimation == null)
+                cacheAnimation = GetComponent<Animation>();
+            return cacheAnimation;
+        }
+    }
+
+    public override void Show()
+    {
+        isShowing = true;
+        CacheAnimation[animShowParam].normalizedTime = 0f;
+        CacheAnimation[animShowParam].speed = 1f;
+        CacheAnimation.Play(animShowParam, PlayMode.StopAll);
+    }
+
+    public override void Hide()
+    {
+        isShowing = false;
+        CacheAnimation[animShowParam].normalizedTime = 1f;
+        CacheAnimation[animShowParam].speed = -1f;
+        CacheAnimation.Play(animShowParam, PlayMode.StopAll);
+    }
+
+    public override bool IsVisible()
+    {
+        return isShowing;
+    }
+}
